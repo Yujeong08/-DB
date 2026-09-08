8b917bfc118eaa22ce39290816f760efc695c203
Yujeong08 <77600847+Yujeong08@users.noreply.github.com>
2026-09-08 13:39:27 +0900

    Point inventory app to v4 Apps Script backend

---
+++ backend-v4.js
@@ -0,0 +1,4 @@
+(()=>{
+  const V4_API='https://script.google.com/macros/s/AKfycbxGzspFRvippjbn7Dl84K2gfiASGw71N5i7CuuJj9rKGf-ZrorTijmHTXFTHWqmgfY6wA/exec';
+  localStorage.setItem('inventory_api_url', V4_API);
+})();
